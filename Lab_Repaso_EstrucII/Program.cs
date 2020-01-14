a07850efcc29b61f77b08a4b9fea404cb3c9e6b3
waloro <walterof1@hotmail.com>
2020-01-14 16:42:36 -0600

    create Lab_Repo
    Hello world!

---
+++ Lab_Repaso_EstrucII/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_Repaso_EstrucII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
