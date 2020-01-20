46458e44371fa9a3d0b788ccab95a2e221711677
waloro <walterof1@hotmail.com>
2020-01-20 14:33:58 -0600

    method get and post
    we need to review it.

---
+++ API/Class/Movies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Class
+{
+    public class Movies
+    {
+        public string Name { get; set; }
+        public string Year { get; set; }
+        public string Director { get; set; }
+    }
+}
--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -35,5 +35,20 @@ namespace API.Controllers
             })
             .ToArray();
         }
+
+
+        [HttpPost]
+        public IEnumerable<WeatherForecast> Post()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
     }
 }
